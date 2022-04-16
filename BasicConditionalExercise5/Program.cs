abca163890586b4ca7645736908630c32edbc48e
Devllu <dev.limaluciano@gmail.com>
2022-04-16 14:58:13 -0300

    Add project files.

---
+++ BasicConditionalExercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿// 21) Faça um algoritmo que leia um determinado ano e mostre se ele é ou não BISSEXTO.
+
+Console.WriteLine("Digite o ano e saiba se ele é bissexto ou não:");
+int year = int.Parse(Console.ReadLine());
+
+if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+{
+    Console.WriteLine($"O ano de {year} é BISSEXTO e tem 366 dias");
+
+}
+else if (year % 4 == 0 && year % 100 != 0)
+{
+    Console.WriteLine($"O ano de {year} é BISSEXTO e tem 366 dias");
+}
+else
+{
+    Console.WriteLine($"O ano de {year} não é BISSEXTO e tem 365 dias");
+}
